ceb6cbdd1f368a316a00e28d4e7dc338e57f24d4
Carsten Stein <carsten.stein@cast-it.de>
2025-06-19 18:15:38 +0200

    feat: add stories

---
+++ packages/ui/src/components/login-form.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { LoginForm } from './login-form';
+
+const meta = {
+  title: 'Forms/LoginForm',
+  component: LoginForm,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: [ 'autodocs' ],
+  args: {
+    username: {
+      label: 'Username',
+      placeholder: 'Enter your username',
+    },
+    password: {
+      label: 'Password',
+      placeholder: 'Enter your password',
+    },
+  }
+} satisfies Meta<typeof LoginForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+};
