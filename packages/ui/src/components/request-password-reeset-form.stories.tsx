5a658c48d44474a659114a1f754ff843d9de388d
Carsten Stein <carsten.stein@cast-it.de>
2025-07-01 12:47:25 +0200

    feat: request password reset

---
+++ packages/ui/src/components/request-password-reeset-form.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { RequestPasswordReset } from "./request-password-reset-form";
+import { fn } from 'storybook/test';
+import { action } from 'storybook/actions';
+
+const meta = {
+  title: "Forms/RequestPasswordReset",
+  component: RequestPasswordReset,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: {
+    onError: fn(),
+    classNames: {
+      root: "w-full max-w-sm",
+      form: "flex flex-col",
+      field: "my-4"
+    }
+  },
+} satisfies Meta<typeof RequestPasswordReset>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    onSubmit: async (values) => {
+      action('onSubmit')(values);
+      return Promise.resolve();
+    }
+  }
+};
+
---
+++ packages/ui/src/components/request-password-reset-form.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { FC } from 'react';
+import { FieldErrors, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import clsx from 'clsx';
+import { Email } from '@cstn/validation/email';
+import { useFormTranslations } from '@cstn/i18n/hooks/useFormTranslations';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@cstn/ui/components/form';
+import { Button } from '@cstn/ui/components/button';
+import { Input } from '@cstn/ui/components/input';
+import { PropsWithStyle } from '@cstn/ui/props';
+
+type Props = PropsWithStyle & {
+  onError?: (errors: FieldErrors) => void;
+  onSubmit: (values: z.infer<typeof FormSchema>) => Promise<void>;
+};
+
+const FormSchema = z.object({
+  email: Email,
+});
+
+export const RequestPasswordReset: FC<Props> = ({ className, classNames, onSubmit, onError }) => {
+  const t = useFormTranslations();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
+
+  const handleSubmit = async (values: z.infer<typeof FormSchema>) => {
+    try {
+      await onSubmit(values);
+    } catch (ex) {
+      form.setError('root', {
+        message: (ex as Error).message,
+      });
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form className={clsx('w-full max-w-sm', className, classNames?.root)}
+            onSubmit={form.handleSubmit(handleSubmit, onError)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className={classNames?.field}>
+              <FormLabel className={classNames?.label}>{t('email.label')}</FormLabel>
+              <FormControl className={classNames?.control}>
+                <Input autoComplete="email" className={classNames?.input}
+                       placeholder={t('email.placeholder')} {...field} />
+              </FormControl>
+              <FormDescription className={classNames?.description}>{t('email.description')}</FormDescription>
+              <FormMessage className={classNames?.message}/>
+            </FormItem>
+          )}
+        />
+        {form.formState.errors.root && (
+          <FormMessage className="my-4">
+            {t(form.formState.errors.root.message || 'form.failed')}
+          </FormMessage>
+        )}
+        <Button disabled={form.formState.isSubmitting} type="submit">{t('requestPasswordReset.submit')}</Button>
+      </form>
+    </Form>
+  );
+};
